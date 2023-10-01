7ffd5f040d5d4d9510f5c521b3142a5243c9d8b6
Olga Agapova <ermolina.olga.l@gmail.com>
2023-10-01 19:12:09 +0300

    Initial commit

---
+++ src/test/java/Task1/MyTest.java
@@ -0,0 +1,9 @@
+package Task1;
+
+public class MyTest {
+    public static void main(String[] args) {
+        int x = -10;
+        assert x >= 0: "x has to be not negative";
+        System.out.println("Program goes on");
+    }
+}
